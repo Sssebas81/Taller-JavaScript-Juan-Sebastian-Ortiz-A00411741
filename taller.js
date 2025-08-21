df11fcd186fc6617237330ec2ccf8663c4595b5b
Sssebas81 <jsebastianortiz81@gmail.com>
2025-08-21 08:15:05 -0500

    Ejercicio 1.1

---
+++ taller.js
@@ -0,0 +1,12 @@
+// Ejercicio 1.1
+// Declarar una variable con var
+var nombre = "Sebastian";
+
+// Mostrar el primer valor
+console.log("Primer nombre:", nombre);
+
+// Reasignar un nuevo valor
+nombre = "Juan";
+
+// Mostrar el nuevo valor
+console.log("Segundo nombre:", nombre);
